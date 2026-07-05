a01c78574a4265db8fc407bafa94ad0337d51c3f
Ducki67 <sabel200901@gmail.com>
2026-07-05 16:45:38 +0200

    Erbium to Boron

--- Erbium/Erbium/Private/dllmain.cpp
+++ Erbium/Erbium/Private/dllmain.cpp
@@ -152,8 +152,8 @@ void Main()
         curl_global_init(CURL_GLOBAL_ALL);
 
     sprintf_s(GUI::windowTitle,
-              VersionInfo.EngineVersion >= 5.0 ? "Erbium (FN %.2f, UE %.1f): Setting up"
-                                               : (VersionInfo.FortniteVersion >= 5.00 || VersionInfo.FortniteVersion < 1.2 ? "Erbium (FN %.2f, UE %.2f): Setting up" : "Erbium (FN %.1f, UE %.2f): Setting up"),
+              VersionInfo.EngineVersion >= 5.0 ? "Boron (FN %.2f, UE %.1f): Setting up"
+                                               : (VersionInfo.FortniteVersion >= 5.00 || VersionInfo.FortniteVersion < 1.2 ? "Boron (FN %.2f, UE %.2f): Setting up" : "Boron (FN %.1f, UE %.2f): Setting up"),
               VersionInfo.FortniteVersion, VersionInfo.EngineVersion);
     SetConsoleTitleA(GUI::windowTitle);
 
